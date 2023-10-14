5cca4de5157f883b3d6e39ddc777631ef92e1400
n3rdium <n3rdium@gmail.com>
2023-10-14 18:46:43 +0530

    Now we can find matches for tri!

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ with open('DeepStacker.log', 'w') as f:
     f.write("")
 # TODO: Create an image list handler ('DSSequence')
 
-from dsimage import DSImage
+from PIL import Image, ImageDraw
 from util_loader import utils
+from dsimage import DSImage
 import numpy as np
 
 image = DSImage()
@@ -18,7 +19,6 @@ objects = utils["StarfinderUtil"].detect(image, {"thresh": 32})
 obj_coords = np.array([objects.data['x'], objects.data['y'], objects.data['a'], objects.data['b']])
 
 # Mark the objects
-from PIL import Image, ImageDraw
 im = Image.open('./example_images/example.jpg')
 draw = ImageDraw.Draw(im)
 fct = 2
@@ -27,7 +27,7 @@ for obj in obj_coords.T:
 im.save("./example_images/result.jpg")
 
 # Format the objects into a dataframe
-tris = utils["StarfinderUtil"].group(objects, {})
+tris = utils["StarfinderUtil"].group(objects)
 
 # TODO: Do the same processes with different thresholds for the object detector to get more variety in structure (size of groups of objects)
 # TODO: Higher thresholds give larger triangle structures, lower thresholds give smaller triangle structures
@@ -37,3 +37,8 @@ tris = utils["StarfinderUtil"].group(objects, {})
 # Now analyze the triangles for their side length ratios and angles and store them in a dataframe
 analyzed = utils["StarfinderUtil"].analyze(tris)
 print(analyzed.head())
+print(analyzed.iloc[0])
+
+# Now find similar triangles
+similar = utils["StarfinderUtil"].find_matches(analyzed.iloc[0], analyzed, 8)
+print(similar.head(8))
--- utils/starfinder.py
+++ utils/starfinder.py
@@ -1,18 +1,15 @@
-from uuid import uuid4
-import time
-
-import sep
 from utils.base_util import BaseUtil
+from scipy.spatial import distance
+from scipy.spatial import KDTree
+from math import acos, degrees
 from dsimage import DSImage
 from logger import logger
-import pandas as pd
-import tqdm
-from scipy.spatial import KDTree
-import numpy as np
-from scipy.spatial import distance
-from math import acos, degrees
-import pandas as pd
 from uuid import uuid4
+import pandas as pd
+import numpy as np
+import time
+import tqdm
+import sep
 
 class DSObjects:
     def __init__(self, id: (uuid4, str), data):
@@ -51,7 +48,7 @@ class StarfinderUtil(BaseUtil):
         logger.debug(f"[{__name__}] Detected objects in {image.id} in {toc-tic} seconds.")
         return objects
     
-    def group(self, objects: DSObjects, group_params: dict):
+    def group(self, objects: DSObjects):
         """
         Group objects into triangles and spot out the triangle vertex coords, angles formed, and the ratio of the side lengths.
         """
@@ -59,7 +56,6 @@ class StarfinderUtil(BaseUtil):
         for i in objects.data:
             arr.append(list(i))
         objects.data = pd.DataFrame(arr[1:], columns=objects.data.dtype.names)
-        objects.data['uuid'] = [uuid4() for i in tqdm.trange(len(objects.data))]
         
         tree = objects.data[['x', 'y']].to_numpy()
         _tree = tree.copy()
@@ -80,7 +76,7 @@ class StarfinderUtil(BaseUtil):
         """
         Analyze the triangles for their side length ratios and angles and store them in a dataframe
         """
-        tris_header = ["uuid", "side_1", "side_2", "side_3", "angle_1", "angle_2", "angle_3"]
+        tris_header = ["id", "side1", "side2", "side3", "angle1", "angle2", "angle3", "absolute_distances"]
         tris_data = []
 
         for tri in tris:
@@ -89,18 +85,46 @@ class StarfinderUtil(BaseUtil):
             distances = [distance.euclidean(tri[0], tri[1]), distance.euclidean(tri[1], tri[2]), distance.euclidean(tri[2], tri[0])]
             # Now convert the distances to ratios
             total = sum(distances)
-            distances = [i / total for i in distances]
+            distance_ratio = [i / total for i in distances]
             
             # Get the angles between the points
             angles = [degrees(acos((distances[0]**2 + distances[2]**2 - distances[1]**2) / (2 * distances[0] * distances[2]))),
                     degrees(acos((distances[0]**2 + distances[1]**2 - distances[2]**2) / (2 * distances[0] * distances[1]))),
                     degrees(acos((distances[1]**2 + distances[2]**2 - distances[0]**2) / (2 * distances[1] * distances[2])))]
             # Store the data
-            tris_data.append([uuid4(), distances[0], distances[1], distances[2], angles[0], angles[1], angles[2]])
+            tris_data.append([len(tris_data), distance_ratio[0], distance_ratio[1], distance_ratio[2], angles[0], angles[1], angles[2], distances])
             
         tris_df = pd.DataFrame(tris_data, columns=tris_header)
-        tris_df.set_index("uuid", inplace=True)
+        tris_df.set_index("id", inplace=True)
         
         return tris_df
+    
+    def _similarity (self, tri1, tri2):
+        """
+        Returns a similarity score between two triangles, between 0 and 1.
+        """
+        return (
+            (
+                abs(tri1['side1'] - tri2['side1']) +
+                abs(tri1['side2'] - tri2['side2']) +
+                abs(tri1['side3'] - tri2['side3'])
+            ) + 
+            (
+                abs(tri1['angle1'] - tri2['angle1']) + 
+                abs(tri1['angle2'] - tri2['angle2']) + 
+                abs(tri1['angle3'] - tri2['angle3'])
+            ) / (180 * 3)
+        ) / 6
+    
+    def find_matches(self, tri, tris_df: pd.DataFrame, n_matches: int):
+        """
+        Analyze a triangle and find the closest match in the tris_df dataframe.
+        """
+        similarities = {}
+        for _tri in tqdm.trange(len(tris_df), desc="Finding matches for tri"):
+            similarities[_tri] = self._similarity(tri, tris_df.iloc[_tri])
+        ret = sorted(similarities, key=similarities.get)[:n_matches]
+        # Return in a df
+        return tris_df.iloc[ret]
 
 EXPORT_UTIL = StarfinderUtil
